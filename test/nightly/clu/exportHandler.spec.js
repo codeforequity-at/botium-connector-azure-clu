e8d204657f7645bc6621002554662ab25495297b
Attila Ujj <attila.ujj@botium.at>
2023-06-22 09:57:43 +0200

    BOT-3974 nightly test

---
+++ test/nightly/clu/exportHandler.spec.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+const { exportHandler } = require('../../../src/intents')
+const { readCaps } = require('./helper')
+
+describe('exporthandler', function () {
+  beforeEach(async function () {
+    this.caps = readCaps()
+  })
+  it('should successfully upload existing utterances', async function () {
+    await exportHandler({ caps: this.caps }, {
+      utterances: [
+        {
+          name: 'greet',
+          utterances: ['Hi', 'Hey']
+        }
+      ]
+    }, {
+      statusCallback: (data) => console.log(data)
+    })
+  }).timeout(50000)
+})
